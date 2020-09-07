b8f471799095ea323a58353dca896b57eb93c867
ShotAlex <shot.aleks@gmail.com>
2020-09-07 23:17:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length < 1) return 0;
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+    if (array.length < 1) return 0;
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+    if (array.length < 1) return 0;
+
+    let len = array.length;
+    let total = array.reduce((acc, item) => {
+      return acc + item;
+  })
+
+  return total / len;
 }
